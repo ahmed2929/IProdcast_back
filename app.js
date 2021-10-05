dc1e519d431f69d8cfa6c5a634e0387571d49427
ahmed2929 <mywork304@gmail.com>
2021-10-05 19:26:27 +0200

    config db connection

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const {startServer} =require('./server');
 const {setMeiddlewares} =require("./middleware/index")
-const ConnectDB=require("./database/mongo/config/index")
+const DB=require("./database/config/index")
 require("dotenv").config();
+const DB_Config={
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
+    
+};
+
+
 (async function () {
+try {
+    let app = await startServer(process.env.PORT || 3355);
+
+    app=setMeiddlewares(app);
+   
+   await DB.connect(DB_Config);
+   console.log("Database connected");
+
+} catch (error) {
+    console.log(error);  
+}
+
+
  
-let app = await startServer(process.env.PORT || 3000);
 
-app=setMeiddlewares(app);
-await ConnectDB()
-
-
-})()
+})();
   
